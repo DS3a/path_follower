a372a6c377048a6a77a04b816db62c3681abf2d0
DS3a <33351122+DS3a@users.noreply.github.com>
2023-03-31 00:47:00 +0530

    make a new path struct instead of implementing over nav_msgs::msg::Path

---
+++ src/path.rs
@@ -0,0 +1,51 @@
+use nalgebra::Vector2;
+use nav_msgs::msg::Path as PathMsg;
+
+fn get_dist(a: Vector2<f64>, b: Vector2<f64>) -> f64 {
+    ((a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2)).powf(0.5)
+}
+
+pub struct Path {
+    path_vector: Vec<Vector2<f64>>,
+}
+
+impl Path {
+    pub fn new(path_msg: PathMsg) -> Self {
+        let mut path_vector = Vec::<Vector2<f64>>::new();
+        for pose_index in 0..(path_msg.poses.len()) {
+            path_vector.push(Vector2::new(
+                path_msg.poses[pose_index].pose.position.x,
+                path_msg.poses[pose_index].pose.position.y,
+            ));
+        }
+
+        Self { path_vector }
+    }
+
+    pub fn find_closest_point(&self, point: Vector2<f64>) -> (Vector2<f64>, usize) {
+        let mut min_dist: Option<f64> = None;
+        let mut min_dist_ind: usize = 0;
+
+        for pose_index in 0..(self.path_vector.len()) {
+            match &mut min_dist {
+                Some(dist_) => {
+                    let dist = get_dist(point, self.path_vector[pose_index]);
+                    if (*dist_ > dist) {
+                        min_dist = Some(dist);
+                        min_dist_ind = pose_index;
+                    }
+                }
+                None => {
+                    min_dist = Some(get_dist(point, self.path_vector[pose_index]));
+                }
+            }
+        }
+
+        println!(
+            "the minimum distance is {:?}, and the index is {}",
+            min_dist, min_dist_ind
+        );
+
+        (self.path_vector[min_dist_ind], min_dist_ind)
+    }
+}
--- src/pure_pursuit.rs
+++ src/pure_pursuit.rs
@@ -1,35 +1,68 @@
-use anyhow::{Result, Error};
-use rclrs;
-use nav_msgs::msg::Path as PathMsg;
+use anyhow::{Error, Result};
 use nav_msgs::msg::Odometry as OdometryMsg;
-use std::thread;
+use nav_msgs::msg::Path as PathMsg;
+use rclrs;
 use std::env;
 use std::sync::{Arc, Mutex};
+use std::thread;
+
+use dynamics_solver;
+mod path;
+
+use nalgebra::Vector2;
+
+// dt = lookahead time
+static dt: f64 = 0.5;
 
 fn main() -> Result<(), Error> {
     let context = rclrs::Context::new(env::args())?;
     let mut node = rclrs::create_node(&context, "pratham_path_follower")?;
 
-    let path_ptr: Arc<Mutex<Option<PathMsg>>> = Arc::new(Mutex::new(Option::<PathMsg>::None));
+    let path_ptr: Arc<Mutex<Option<path::Path>>> = Arc::new(Mutex::new(Option::<path::Path>::None));
+    let odom_ptr: Arc<Mutex<Option<OdometryMsg>>> =
+        Arc::new(Mutex::new(Option::<OdometryMsg>::None));
 
     let path_subscription_path_ptr = Arc::clone(&path_ptr);
-    let path_subscription = node
-        .create_subscription("/path_local",
-                             rclrs::QOS_PROFILE_DEFAULT,
-                             move |msg: PathMsg| {
-                                 /*
-                                 println!("received path of type {:?}", &msg.poses.len());
-                                 println!("received path begin {:?}", &msg.poses[0]);
-                                 println!("received path end {:?}", &msg.poses[&msg.poses.len()-3]);
-                                 println!("received path end {:?}", &msg.poses[&msg.poses.len()-2]);
-                                 println!("received path end {:?}\n\n\n", &msg.poses[&msg.poses.len()-1]);
-                                 */
-                                 println!("received path");
-                                 *path_subscription_path_ptr.lock().unwrap() = Some(msg);
-                             },);
+    let path_subscription_odom_ptr = Arc::clone(&odom_ptr);
+    let path_subscription = node.create_subscription(
+        "/path_local",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: PathMsg| {
+            println!("received path");
+            /*
+             * TODO
+             * store reference odom
+             * store the path vector DONE
+             */
+            // *path_subscription_path_ptr.lock().unwrap() = Some(msg);
+            let path_to_follow = path::Path::new(msg);
+            path_to_follow.find_closest_point(Vector2::new(0.2f64, 0f64));
+            *path_subscription_path_ptr.lock().unwrap() = Some(path_to_follow);
+            if let Some(odom) = &*path_subscription_odom_ptr.lock().unwrap() {
+                // TODO set reference odom
+            } else {
+                println!("The odom pointer is None, please make sure the odometry topic is being published to")
+            }
+        },
+    );
+
+    let odom_subscription_odom_ptr = Arc::clone(&odom_ptr);
+    let odom_subscription = node.create_subscription(
+        "/Odometry/local",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: OdometryMsg| {
+            *odom_subscription_odom_ptr.lock().unwrap() = Some(msg);
+        },
+    );
+
+    thread::spawn(move || {
+        /*
+         * TODO follow the path
+         *
+         */
+    });
 
     loop {
         rclrs::spin(&node)?;
     }
-    Ok(())
 }
