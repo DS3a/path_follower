72a9c4bd452fbb7aea1e6a0d6f70274a0b39c504
DS3a <33351122+DS3a@users.noreply.github.com>
2023-04-03 01:05:12 +0530

    need to test

--- src/path.rs
+++ src/path.rs
@@ -5,14 +5,19 @@ fn get_dist(a: Vector2<f64>, b: Vector2<f64>) -> f64 {
     ((a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2)).powf(0.5)
 }
 
-fn perpendicular_distance(point_a: &Vector2<f64>, point_b: &Vector2<f64>, point_c: &Vector2<f64>) -> f64 {
+fn perpendicular_distance(
+    point_a: &Vector2<f64>,
+    point_b: &Vector2<f64>,
+    point_c: &Vector2<f64>,
+) -> f64 {
     // Find the equation of the line joining point_a and point_b
     let slope = (point_b.y - point_a.y) / (point_b.x - point_a.x);
     let y_intercept = point_a.y - slope * point_a.x;
 
     println!("The line equation is y = {}x + {}", &slope, &y_intercept);
 
-    let dist: f64 = (point_c.x - (slope * point_c.y) - y_intercept) / (1f64 + slope.powi(2)).powf(0.5);
+    let dist: f64 =
+        (-(slope * point_c.x) + point_c.y - y_intercept) / (1f64 + slope.powi(2)).powf(0.5);
     println!("The distance between point_c and the line is : {}", &dist);
     dist
 }
@@ -34,11 +39,15 @@ impl Path {
         Self { path_vector }
     }
 
-    pub fn find_closest_point(&self, point: Vector2<f64>) -> (Vector2<f64>, usize) {
+    pub fn find_closest_point(
+        &self,
+        point: Vector2<f64>,
+        start_ind_option: Option<usize>,
+    ) -> (Vector2<f64>, usize) {
         let mut min_dist: Option<f64> = None;
         let mut min_dist_ind: usize = 0;
 
-        for pose_index in 0..(self.path_vector.len()) {
+        for pose_index in start_ind_option.unwrap_or(0)..(self.path_vector.len()) {
             match &mut min_dist {
                 Some(dist_) => {
                     let dist = get_dist(point, self.path_vector[pose_index]);
@@ -61,9 +70,41 @@ impl Path {
         (self.path_vector[min_dist_ind], min_dist_ind)
     }
 
-    // TODO finish this function
-    pub fn get_path_deviation(&self, state: Vector2<f64>) -> (bool, f64) {
+    // get the displacement of the state from the path
+    pub fn get_path_deviation(&self, state: Vector2<f64>) -> Option<f64> {
+        let (closest_point, closest_point_idx) = self.find_closest_point(state.clone(), None);
+        if closest_point_idx == self.path_vector.len() - 1 {
+            println!("The goal has been reached, line can't be formed");
+            None
+        } else {
+            let point_a = closest_point.clone();
+            let point_b = self.path_vector[closest_point_idx + 1].clone();
+            Some(perpendicular_distance(&point_a, &point_b, &state))
+        }
+    }
 
+    pub fn get_propagtation_deviation(
+        &self,
+        state: Vector2<f64>,
+        future_state: Vector2<f64>,
+    ) -> Option<f64> {
+        let (starting_point, starting_point_idx) = self.find_closest_point(state.clone(), None);
+
+        // according to the control flow, this will probably never be reached
+        if starting_point_idx == self.path_vector.len() - 1 {
+            println!("The goal has been reached, line can't be formed");
+            None
+        } else {
+            let (goal_point, goal_idx) =
+                self.find_closest_point(future_state.clone(), Some(starting_point_idx));
+            if goal_idx == self.path_vector.len() - 1 {
+                None
+            } else {
+                let point_a = goal_point;
+                let point_b = self.path_vector[goal_idx + 1].clone();
+                Some(perpendicular_distance(&point_a, &point_b, &future_state))
+            }
+        }
     }
 
     pub fn get_path_size(&self) -> usize {
--- src/pure_pursuit.rs
+++ src/pure_pursuit.rs
@@ -1,4 +1,5 @@
 use anyhow::{Error, Result};
+use geometry_msgs::msg::Twist as TwistMsg;
 use nav_msgs::msg::Odometry as OdometryMsg;
 use nav_msgs::msg::Path as PathMsg;
 use rclrs;
@@ -13,11 +14,24 @@ use nalgebra::Vector2;
 use nalgebra::{Quaternion, UnitQuaternion};
 
 // dt = lookahead time
-static DT: f64 = 0.5;
+static DT: f64 = 0.95492;
+static DEVIATION_THRESHOLD: f64 = 0.3; // in meters
+static LOOKAHEAD_DISCOUNT_FACTOR: f64 = 1.5;
 
+static DEACCELERATION_DECAY: f64 = 0.5;
+
+static MIN_LIN_X: f64 = 0.443;
+static MAX_LIN_X: f64 = 1.0;
+
+static MIN_LIN_X_FOR_ROT: f64 = 0.2235;
+
+static MAX_ANG_Z: f64 = 1.0471975;
+
+static HARSH_GAIN: f64 = 1.0;
+static MEDIUM_GAIN: f64 = 0.5;
+static DAMPED_GAIN: f64 = 0.25;
 
 fn main() -> Result<(), Error> {
-
     let context = rclrs::Context::new(env::args())?;
     let mut node = rclrs::create_node(&context, "pratham_path_follower")?;
 
@@ -28,6 +42,14 @@ fn main() -> Result<(), Error> {
     let reference_odom_ptr: Arc<Mutex<Option<OdometryMsg>>> =
         Arc::new(Mutex::new(Option::<OdometryMsg>::None));
 
+    let cmd_vel_publisher_ptr = Arc::new(Mutex::new(
+        node.create_publisher::<TwistMsg>("/cmd_vel", rclrs::QOS_PROFILE_DEFAULT)?,
+    ));
+
+    let max_lin_x_ptr = Arc::new(Mutex::new(MAX_LIN_X));
+    let min_lin_x_ptr = Arc::new(Mutex::new(MIN_LIN_X));
+
+    let max_ang_z_ptr = Arc::new(Mutex::new(MAX_ANG_Z));
 
     let path_subscription_path_ptr = Arc::clone(&path_ptr);
     let path_subscription_odom_ptr = Arc::clone(&odom_ptr);
@@ -52,6 +74,10 @@ fn main() -> Result<(), Error> {
     let odom_subscription_odom_ptr = Arc::clone(&odom_ptr);
     let odom_subscription_path_ptr = Arc::clone(&path_ptr);
     let odom_subscription_reference_odom_ptr = Arc::clone(&reference_odom_ptr);
+    let odom_subscription_max_ang_z_ptr = Arc::clone(&max_ang_z_ptr);
+    let odom_subscription_max_lin_x_ptr = Arc::clone(&max_lin_x_ptr);
+    let odom_subscription_min_lin_x_ptr = Arc::clone(&min_lin_x_ptr);
+    let odom_subscription_cmd_vel_publisher_ptr = Arc::clone(&cmd_vel_publisher_ptr);
     let _odom_subscription = node.create_subscription(
         "/Odometry/local",
         rclrs::QOS_PROFILE_DEFAULT,
@@ -88,30 +114,83 @@ fn main() -> Result<(), Error> {
                 None => {
                     println!("The reference odom has not been set, i.e., the path callback has not been called, is the path being published?");
                     // setting reference odom to current odom for debugging purposes
-                    *reference_odom = Some(msg);
+                    *reference_odom = Some(msg.clone());
                 }
             }
 
             std::mem::drop(reference_odom);
             println!("The current state is : {:?}", &current_state);
 
+            let mut vel_cmd = TwistMsg::default();
+
             // now to find the closest point in the path to the the current state
             let path_to_follow_opt = & *odom_subscription_path_ptr.lock().unwrap();
             match path_to_follow_opt {
                 Some(path_to_follow) => {
-                    let (closest_point, idx) = path_to_follow.find_closest_point(Vector2::new(current_state.x, current_state.y));
-                    println!("The closest point is {}, and it is in the {} place in the path vector", &closest_point, &idx);
+                    println!("The twist msg is {:?}", vel_cmd);
+                    match path_to_follow.get_path_deviation(Vector2::new(current_state.x, current_state.y)) {
+                        Some(deviation) => {
+                            if deviation > DEVIATION_THRESHOLD {
+                                vel_cmd.linear.x = MIN_LIN_X_FOR_ROT;
+                                vel_cmd.angular.z = HARSH_GAIN * deviation;
+                            } else {
+                                /*
+                                 * TODO
+                                 * propagate the state
+                                 * check if the propagated state is in the path
+                                 ** if it is, then check deviation and stuff, and adjust the angular velocity with medium/low gain
+                                 ** if it is not,
+                                 */
 
-                    if idx == path_to_follow.get_path_size() - 1 {
-                        println!("The goal has been reached");
-                    } else {
+                                let mut propagated_state = current_state.clone().propagate(DT);
+                                match path_to_follow.get_propagtation_deviation(Vector2::new(current_state.x, current_state.y),
+                                                                                Vector2::new(propagated_state.x, propagated_state.y)) {
+                                    Some(deviation) => {
+                                        if deviation > DEVIATION_THRESHOLD * LOOKAHEAD_DISCOUNT_FACTOR {
+                                            vel_cmd.linear.x = *odom_subscription_min_lin_x_ptr.lock().unwrap();
+                                            vel_cmd.angular.z = MEDIUM_GAIN * deviation;
+                                        } else {
+                                            vel_cmd.angular.z = DAMPED_GAIN * deviation;
+                                            let min_lin_x = *odom_subscription_min_lin_x_ptr.lock().unwrap();
+                                            let max_lin_x = *odom_subscription_max_lin_x_ptr.lock().unwrap();
 
+                                            vel_cmd.linear.x = min_lin_x + (max_lin_x - min_lin_x) * deviation / (DEVIATION_THRESHOLD * LOOKAHEAD_DISCOUNT_FACTOR);
+
+                                            std::mem::drop(min_lin_x);
+                                            std::mem::drop(max_lin_x);
+                                        }
+                                    },
+                                    None => { // if the propagated state is near/beyond the goal; start deceleration
+                                        vel_cmd.linear.x = &msg.twist.twist.linear.x * DEACCELERATION_DECAY;
+                                        vel_cmd.angular.z = &msg.twist.twist.angular.z * DEACCELERATION_DECAY;
+                                    }
+                                }
+
+                            }
+                        }, None => {
+                            println!("The goal has been reached, sending stop command");
+                        }
                     }
                 },
                 None => {
-                    println!("The path is empty, is it being published?");
+                    println!("The path is empty, is it being published?, sending stop command");
                 },
             }
+
+            let max_lin_x = *odom_subscription_max_lin_x_ptr.lock().unwrap();
+            let max_ang_z = *odom_subscription_max_ang_z_ptr.lock().unwrap();
+
+            if vel_cmd.linear.x.abs() > max_lin_x {
+                vel_cmd.linear.x = max_lin_x * vel_cmd.linear.x/vel_cmd.linear.x.abs();
+                std::mem::drop(max_lin_x);
+            }
+
+            if vel_cmd.angular.z.abs() > max_ang_z {
+                vel_cmd.angular.z = max_ang_z * vel_cmd.angular.z/vel_cmd.angular.z.abs();
+                std::mem::drop(max_ang_z);
+            }
+
+            odom_subscription_cmd_vel_publisher_ptr.lock().unwrap().publish(vel_cmd).unwrap();
         },
 
     );
