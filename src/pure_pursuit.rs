1191c554f92d91aa212334c523636881c0a529ee
DS3a <33351122+DS3a@users.noreply.github.com>
2023-04-13 01:27:37 +0530

    make changes to gains after testing

--- src/pure_pursuit.rs
+++ src/pure_pursuit.rs
@@ -16,7 +16,7 @@ use nalgebra::{Quaternion, UnitQuaternion};
 // dt = lookahead time
 static DT: f64 = 1.1492;
 static DEVIATION_THRESHOLD: f64 = 0.025; // in meters
-static LOOKAHEAD_DISCOUNT_FACTOR: f64 = 1.0;
+static LOOKAHEAD_DISCOUNT_FACTOR: f64 = 1.1;
 
 static DEACCELERATION_DECAY: f64 = 0.5;
 
@@ -27,9 +27,9 @@ static MIN_LIN_X_FOR_ROT: f64 = 0.2235;
 
 static MAX_ANG_Z: f64 = 1.0471975;
 
-static HARSH_GAIN: f64 = -5.1;
-static MEDIUM_GAIN: f64 = -3.30;
-static DAMPED_GAIN: f64 = -1.25;
+static HARSH_GAIN: f64 = -2.8;
+static MEDIUM_GAIN: f64 = -2.20;
+static DAMPED_GAIN: f64 = -1.225;
 
 fn main() -> Result<(), Error> {
     let context = rclrs::Context::new(env::args())?;
